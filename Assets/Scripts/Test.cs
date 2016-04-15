ef58147a2b1d90d0ca80e5f01cf74ddae21bc6b4
wasd125 <michael.koehler.wasd@googlemail.com>
2016-04-15 20:56:13 +0200

    Der Arsch

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+    private int fotz;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,6 @@ public class Test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.Log("uwe");
 	}
 }
