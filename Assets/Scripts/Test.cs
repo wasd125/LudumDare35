b1e2559677c67170d945abcfa3fb0e5e9c2cf3ae
M4lph <malph@live.de>
2016-04-15 20:54:19 +0200

    test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Test : MonoBehaviour {
 
+	private string schwanz;
 	// Use this for initialization
 	void Start () {
 	
+
 	}
 	
 	// Update is called once per frame
