ee160b33c19f855f757bbc77d71fcf7af83d4ce9
wasd125 <michael.koehler.wasd@googlemail.com>
2016-04-16 12:36:59 +0200

    Scroller

---
+++ Assets/Scripts/ScrollingLayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingLayer : MonoBehaviour {
+
+    public Sprite mySprite;
+
+    public float Multiplier { get; private set; }
+
+    public Vector3 MoveTowardsPos { get; private set; }
+
+    public void InitLayer(float multiplier)
+    {
+        Multiplier = multiplier;
+    }
+
+    public void MoveToPosition(float distance)
+    {
+        MoveTowardsPos = new Vector3(transform.position.x + (distance * Multiplier), transform.position.y, transform.position.z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        if (transform.position != MoveTowardsPos)
+        {
+            transform.position = MoveTowardsPos;
+        }
+
+	}
+}
