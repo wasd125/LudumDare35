31330b84ee8cd476a0a4d035186e29ad2642f85c
wasd125 <michael.koehler.wasd@googlemail.com>
2016-04-17 14:11:19 +0200

    Rolling enemy

--- Assets/Scripts/Enemy_backup.cs
+++ Assets/Scripts/Enemy_backup.cs
@@ -1,36 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy_backup : MonoBehaviour {
 
     public SpriteRenderer rend { get; private set; }
     public Animator anim { get; private set; }
     public Rigidbody2D rb2d { get; private set; }
-	public BoxCollider2D bc2d { get; private set; }
-	public BoxCollider2D gc2d { get; private set; }
-	public CircleCollider2D cc2d { get; private set; }
     private IEnemyBehaviour myBehaviour;
 
-    public enum EnumEnemyType { JumpingEnemy, WalkingEnemy, RollingEnemy, DashingEnemy, DiggingEnemy }
+    public enum EnumEnemyType { JumpingEnemy,WalkingEnemy }
     public EnumEnemyType EnemyType;
 
     public float jumpForce;
     public float walkingSpeed;
-	public float rollingSpeed;
 
     public Sprite AttackSprite;
     public Sprite NormalSprite;
-
-    public float MoveDistance = 3f;
 
     // Use this for initialization
     void Start () {
         rend = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
-		bc2d = GetComponent<BoxCollider2D>();
-		gc2d = GetComponent<BoxCollider2D>();
-		cc2d = GetComponent<CircleCollider2D>();
 
         NormalSprite = rend.sprite;
 
@@ -42,18 +33,9 @@ public class Enemy : MonoBehaviour {
             case EnumEnemyType.WalkingEnemy:
                 myBehaviour = new WalkingEnemy();
                 break;
-			case EnumEnemyType.RollingEnemy:
-				myBehaviour = new RollingEnemyBehaviour();
-				break;
-			//case EnumEnemyType.DashingEnemy:
-			//	myBehaviour = new DashingEnemyBehaviour();
-			//	break;
-			//case EnumEnemyType.DiggingEnemy:
-			//	myBehaviour = new DiggingEnemyBehaviour();
-			//	break;
         }
 
-        myBehaviour.InitBehaviour(this);
+        //myBehaviour.InitBehaviour(this);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/RollingEnemyBehaviour.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollingEnemyBehaviour : IEnemyBehaviour {
+
+	public Enemy MyController;
+	public enum EnumRollingEnemyState { IdleLeft, RollRight, IdleRight, RollLeft }
+	public EnumRollingEnemyState State;
+
+	public float waitForNextState = 0.3f;	
+	public float startPosX;
+	public float moveToX;
+
+	public void DoUpdate()
+	{
+		waitForNextState -= Time.deltaTime;
+		if (waitForNextState > 0) return;
+
+		switch (State)
+		{
+		case EnumRollingEnemyState.IdleLeft:
+			IdleLeft();
+			break;
+		case EnumRollingEnemyState.RollLeft:
+			RollLeft();
+			break;
+		case EnumRollingEnemyState.IdleRight:
+			IdleRight();
+			break;
+		case EnumRollingEnemyState.RollRight:
+			RollRight();
+			break;
+		}
+	}
+
+	void IdleLeft()
+	{	
+		MyController.rend.sprite = MyController.AttackSprite;
+		MyController.gameObject.tag = "DamageSource";
+		MyController.gc2d.enabled = true;
+		MyController.bc2d.enabled = true;
+		MyController.cc2d.enabled = false;
+		moveToX = startPosX + MyController.MoveDistance;
+		State = EnumRollingEnemyState.RollRight;
+		waitForNextState = 0.5f;
+	}
+	void IdleRight()
+	{	
+		MyController.rend.sprite = MyController.AttackSprite;
+		MyController.gameObject.tag = "DamageSource";
+		MyController.gc2d.enabled = true;
+		MyController.bc2d.enabled = true;
+		MyController.cc2d.enabled = false;
+		moveToX = MyController.transform.position.x - MyController.MoveDistance;
+		State = EnumRollingEnemyState.RollLeft;
+		waitForNextState = 0.5f;
+	}
+	void RollLeft()
+	{
+		if (MyController.transform.position.x > moveToX)
+		{
+			MyController.transform.position = Vector3.MoveTowards(MyController.transform.position, new Vector3(moveToX, MyController.transform.position.y, 1), MyController.rollingSpeed * Time.deltaTime);
+			MyController.gc2d.enabled = false;
+			MyController.bc2d.enabled = false;
+			MyController.cc2d.enabled = true;
+			MyController.rend.sprite = MyController.NormalSprite;
+			MyController.gameObject.tag = "Obstacle";
+		}
+		else
+		{
+			State = EnumRollingEnemyState.IdleLeft;
+		}
+	}
+	void RollRight()
+	{
+		if (MyController.transform.position.x < moveToX)
+		{
+			MyController.transform.position = Vector3.MoveTowards(MyController.transform.position, new Vector3(moveToX, MyController.transform.position.y, 1), MyController.rollingSpeed * Time.deltaTime);
+			MyController.gc2d.enabled = false;
+			MyController.bc2d.enabled = false;
+			MyController.cc2d.enabled = true;
+			MyController.rend.sprite = MyController.NormalSprite;
+			MyController.gameObject.tag = "Obstacle";
+
+		}
+		else
+		{
+			State = EnumRollingEnemyState.IdleRight;
+		}
+	}
+
+	public void InitBehaviour(Enemy enemy)
+	{
+		MyController = enemy;
+		startPosX = enemy.transform.position.x;
+	}
+}
