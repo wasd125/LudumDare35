e012c1c3113ecc0e233ab939ebe019e3f8ba11a7
wasd125 <michael.koehler.wasd@googlemail.com>
2016-04-17 12:49:04 +0200

    In

--- Assets/CheckPoint.cs
+++ Assets/CheckPoint.cs
@@ -5,13 +5,11 @@ public class CheckPoint : MonoBehaviour {
 
     void  OnTriggerEnter2D(Collider2D other)
     {       
-        Debug.Log("Triggered");
 
         if (other.tag == "Player")
         {
             if (transform.position.x > LevelController.Instance.SpawnPosition.x)
                 LevelController.Instance.SpawnPosition = transform.position;
-            Debug.Log("Saved");
         }
     }
 }
